9412066a53fa95f41109ecb41f7c66215ba0cd49
Андрей Сельдюков <a.seldukov@onpoint.ru>
2021-12-01 07:33:33 +0300

    init repo

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">Start</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  padding: 24px;
+  box-sizing: border-box;
+}
+
+html,
+body,
+#app {
+  background-color: #e5e5e5;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
